e042025cd005abd34cf60a0a3cf04b1fdc6de547
EitanHemed <37670372+EitanHemed@users.noreply.github.com>
2022-07-21 10:53:34 +0300

    Added tests

--- tspair/_utils.py
+++ tspair/_utils.py
@@ -2,6 +2,7 @@
 tspair is a small library for matching pairs of time-series while maximizing
 similarity on one dimension and maximizing dissimilarity on another.
 """
+import itertools
 import typing
 
 import numpy as np
@@ -31,36 +32,42 @@ def pair_from_array(array: npt.NDArray, maximize: typing.Callable,
 
     array = array.copy()
 
-    max_similarity_aggregated = aggregate_array(array, maximize)
-    min_similarity_aggregated = aggregate_array(array, minimize)
+    max_similarity_aggregated = _aggregate_array(array, maximize)
+    min_similarity_aggregated = _aggregate_array(array, minimize)
 
-    max_func_diffs_mat = get_diffs_matrix(max_similarity_aggregated)
-    min_func_diffs_mat = get_diffs_matrix(min_similarity_aggregated)
+    max_sim_func_diffs_mat = get_diffs_matrix(max_similarity_aggregated)
+    min_sim_func_diffs_mat = get_diffs_matrix(min_similarity_aggregated)
 
-    # For the max_mat we need to maximize similarity (i.e., get the smallest
-    # difference).
-    minimized_differences_vector = np.nanargmin(max_func_diffs_mat, axis=1)
-    # For the min_mat we need to minimize similarity (i.e., get the largest
-    # difference).
-    maximized_differences_vector = np.nanargmax(min_func_diffs_mat, axis=1)
+    normalized_max_sim_diffs_mat = normalize(max_sim_func_diffs_mat)
+    normalized_min_sim_diffs_mat = normalize(min_sim_func_diffs_mat)
 
-    # Normalize all differences between 0 and 1
-    norm_min_diff_vec = normalize(minimized_differences_vector)
-    norm_max_diff_vec = normalize(maximized_differences_vector)
+    # # For the max_mat we need to maximize similarity (i.e., get the smallest
+    # # difference).
+    # minimized_differences_vector = np.nanargmin(max_sim_func_diffs_mat, axis=1)
+    # # For the min_mat we need to minimize similarity (i.e., get the largest
+    # # difference).
+    # maximized_differences_vector = np.nanargmax(min_sim_func_diffs_mat, axis=1)
+    #
+    # # Normalize all differences between 0 and 1
+    # norm_min_diff_vec = normalize(minimized_differences_vector)
+    # norm_max_diff_vec = normalize(maximized_differences_vector)
 
     # Return the index of pairs
-    optimized_differences = optimize(norm_min_diff_vec, norm_max_diff_vec,
+    optimized_differences = optimize(normalized_max_sim_diffs_mat, normalized_min_sim_diffs_mat,
                             maximize_weight, minimize_weight
                             )
 
-    optimal_indices = np.argmax(optimized_differences)
+    optimal_indices = np.nanargmin(optimized_differences.flatten())
+
+    sample_indices = np.arange(array.shape[0])
+    indice_pairs = list(itertools.product(sample_indices, sample_indices))
 
     if return_indices is True:
-        return optimal_indices
+        return indice_pairs[optimal_indices]
 
-    return array[optimal_indices, :]
+    return array[indice_pairs[optimal_indices], :]
 
-def aggregate_array(array: npt.NDArray, func: typing.Callable) -> npt.NDArray:
+def _aggregate_array(array: npt.NDArray, func: typing.Callable) -> npt.NDArray:
     return func(array, axis=1)
 
 def get_diffs_matrix(array: npt.NDArray):
@@ -72,15 +79,16 @@ def get_diffs_matrix(array: npt.NDArray):
     return diffs_mat
 
 def normalize(a: npt.NDArray):
-    return (a + a.min()) / (a.max() - a.min())
+    return (a - np.nanmin(a)) / (np.nanmax(a) - np.nanmin(a))
 
 def optimize(maximize_similarity_array, minimize_similarity_array,
                       maximize_weight, minimize_weight):
     optimized = maximize_similarity_array * maximize_weight + (
-            np.reciprocal(minimize_similarity_array) * minimize_weight)
+            np.reciprocal(minimize_similarity_array) * minimize_weight
+    )
 
     # Handle infinite values introduced by calculating reciprocal of 0
-    optimized[np.isinf(optimized)] = np.nan
+    # optimized[np.isinf(optimized)] = 1
 
     return optimized
 
